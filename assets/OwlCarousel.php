f59e0c259760cce307ce964d2b223960102b60c1
Тимур <timur@Timurs-iMac.local>
2019-02-20 01:11:59 +0500

    owl carousel

--- assets/OwlCarousel.php
+++ assets/OwlCarousel.php
@@ -12,7 +12,10 @@ use yii\web\AssetBundle;
 
 class OwlCarousel extends AssetBundle
 {
-    public $sourcePath = '@vendor/OwlCarousel2/OwlCarousel2/dist';
+    public $sourcePath = '@vendor/owlcarousel2/owlcarousel2/dist';
     public $css = ['assets/owl.carousel.min.css'];
     public $js = ['owl.carousel.min.js'];
+    public $depends = [
+        JQuery::class,
+    ];
 }
