69841596c06f3e2e6d5a6a46b7d6f97bfa3a44e4
Тимур <v.timur8484@gmail.com>
2019-02-24 03:50:52 +0500

    Сортировка блоков

--- modules/site/frontend/controllers/DefaultController.php
+++ modules/site/frontend/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace modules\site\frontend\controllers;
 
 use modules\site\models\Page;
 
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
@@ -22,8 +23,13 @@ class DefaultController extends Controller
     public function actionIndex($alias='index')
     {
         if (($page = Page::findOne(['alias' => $alias])) !== null) {
+            $dataProvider = new ActiveDataProvider([
+                'query' => $page->getPageBlocks()->with('block')->orderBy('order'),
+                'sort' => false,
+            ]);
             return $this->render('index', [
                 'page' => $page,
+                'blockModels' => $dataProvider->models,
             ]);
         }
         throw new NotFoundHttpException();
--- modules/site/frontend/views/default/index.php
+++ modules/site/frontend/views/default/index.php
@@ -2,14 +2,15 @@
 
 /* @var \yii\web\View $this */
 /* @var \modules\site\models\Page $page */
+/* @var \modules\site\models\PageBlock[] $blockModels */
 
 $this->title = $page->title;
 
 ?>
 <section class="section">
     <div class="container">
-        <?php foreach ($page->blocks as $block): ?>
-            <?= $block->content ?>
+        <?php foreach ($blockModels as $model): ?>
+            <?= $model->block->content ?>
         <?php endforeach ?>
     </div>
 </section>
